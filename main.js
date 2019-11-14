76ca5d06b16c169a2fd562411fdbd7e7f57a5d4d
Mushtari Sadia <midnight.scream98@gmail.com>
2019-11-14 18:56:02 +0600

    completed with simple UI

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+let score = 0;
+
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
@@ -7,26 +11,73 @@ function computerPlay() {
     return a;         
 }
 
-function userPlay() {
-    let result = window.prompt("Enter your move").toLowerCase();
-    if(result == "rock" )
-        return 1;
-    else if(result == "paper" )
-        return 2;
-    else if(result == "scissors" )
-        return 3;
-    else return 1 ;
-}
 
 function gamePlay(c,u) {
 
     if(c==u) return "Draw!";
 
-    let g = c + u;
-    if(g == 4) return "Rock destroys Scissors!" ;
-    else if(g == 5) return "Scissors cut Paper!" ;
-    else if(g == 3) return " Paper covers Rock!" ;
+    let g = c.toString() + u.toString();
+    if(g == "13") 
+    {   
+        score--;
+        return "Rock destroys Scissors! You Lose!" ;
+    }
+    else if(g == "31") 
+    {
+        score++;
+        return "Rock destroys Scissors! You Win!" ;
+    }
+    else if(g == "23") 
+    {
+        score++;
+        return "Scissors cut Paper! You Win!" ;
+    }
+    else if(g == "32") 
+    {
+        score--;
+        return "Scissors cut Paper! You Lose!" ;
+    }
+
+    else if(g == "12") 
+    {
+        score++;
+        return " Paper covers Rock! You Win!" ;
+    }
+    else if(g == "21")
+    {
+        score--;
+        return " Paper covers Rock! You Lose!" ;
+    }
 
 }
 
-console.log(gamePlay(computerPlay(),userPlay())) ;
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+    let result;
+
+    if(e.keyCode == 82)
+        result = 1;
+    else if(e.keyCode == 80 )
+        result = 2;
+    else if(e.keyCode == 83 )
+        result = 3;
+
+    document.getElementById("resultBox").innerHTML = gamePlay(computerPlay(),result) ;
+    document.getElementById("scoreTxt").innerHTML = "Score : " + score ;
+    
+  }
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+
+
